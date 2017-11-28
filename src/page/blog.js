4306b0c575c533fcebf45c47d06052c303dfa943
erdivartanovich <erdivartanovich@gmail.com>
2017-11-28 20:19:37 +0700

    change blog content

--- src/page/blog.js
+++ src/page/blog.js
@@ -4,7 +4,7 @@ const Blog = {
     view: function() {
         return m('.mw6 center tc baskerville', [
             m('h1.lh-title', 'Blog as you wish'),
-            m('h4.fw2 bg-gold center br2 tc', 'Will soon create a blog post for software engineering, pray for me! (god, i\'m too busy)'),
+            m('h4.fw2 bg-gold center br2 tc', 'Will soon create a blog post for software engineering, pray for me!'),
             m('a.fw1 br2 tc', {href: '/', oncreate: m.route.link}, 'back to home')
         ])
     }
